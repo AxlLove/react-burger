481fc3baa93d471b94e5c2b3ce540d35ab319b4e
Aleksey Kitaev <axlstarxx@gmail.com>
2022-09-29 22:08:17 +0300

    sprint1/step1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Tab, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 
 function App() {
   return (
     <div className="App">
-
+      <p>test</p>
     </div>
   );
 }
