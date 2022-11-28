840a35b0112462458f8a755957117afc6705cb00
Aleksey Kitaev <axlstarxx@gmail.com>
2022-11-28 19:50:23 +0300

    iteration2

--- src/services/selectors/burgerConstructorSelectors.js
+++ src/services/selectors/burgerConstructorSelectors.js
@@ -4,17 +4,11 @@ const selectOtherIngredient = store => store?.burgerConstructor?.constructorData
 const selectBun = store => store?.burgerConstructor?.bun
 
 export const totalPriceSelector = createSelector(selectBun, selectOtherIngredient, (selectBun, selectOtherIngredient) => {
-        if(selectOtherIngredient) {
             const data = [selectBun, ...selectOtherIngredient, selectBun]
-            return data.reduce((subtotal, item) => {
-                if (!item) {
-                    return subtotal
-                }
-               return  subtotal + item?.price
-            }, 0)
-        }
+            return data.reduce((subtotal, item) => item? subtotal + item?.price : subtotal, 0)
     }
 )
+
 export const constructorDataSelector = (store) => ({
     selectedBun: store.burgerConstructor.bun,
     otherIngredients: store.burgerConstructor.constructorData
