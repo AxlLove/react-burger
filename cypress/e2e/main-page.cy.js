ba745a4a385b34ea2926dae51031a8842e317d46
Алексей Китаев <axlstarxx@gmail.com>
2022-12-23 18:00:29 +0300

    add e2e test

--- cypress/e2e/main-page.cy.js
+++ cypress/e2e/main-page.cy.js
@@ -1,27 +1,35 @@
 describe('burger-constructor e2e', ()=> {
     before(function() {
+        cy.visit('/');
+    });
+
+    it('shoud open and close ingredient modal', ()=>{
+        cy.get('div[id="Булки"]').find('li').first().as('testedBun')
+
+        cy.get('@testedBun').click()
+        
+        cy.get('[class^=IngredientDetails_ingredientDetails__hlRGm').contains("Краторная булка N-200i")
+        cy.url().should('include' , '/ingredients')
+
+        cy.get('button').last().click()
+        cy.url().should("not.include", '/ingredients')
+    })
+
+    it('should auth user make an order', ()=> {
         cy.visit('/login');
         cy.get('input[name="email"]').type('axlstarxx@gmail.com').should('have.value', 'axlstarxx@gmail.com')
         cy.get('input[name="password"]').type('12345').should('have.value', '12345')
         cy.get('button').click()
-    });
+        cy.url().should("not.include", '/login')
+        
+        cy.get('div[id="Булки"]').find('li').first().as('testedBun')
+        cy.get('div[id="Соусы"]').find('li').first().as('testedSauce')
+        cy.get('div[id="Начинки"]').find('li').first().as('testedMain')
 
-
-    it('should open modal page after ingredient click', ()=> {
-        //
-        cy.get('div[id="Булки"]').as('bunList')
-        cy.get('div[id="Соусы"]').as('sauceList')
-        cy.get('div[id="Начинки"]').as('mainList')
-
-        cy.get('@bunList').find('li').first().click()
-        cy.url().should('include' , '/ingredients')
-        cy.get('button').last().click()
-        cy.url().should("not.include", '/ingredients')
-        //TODO вынести
         const dataTransfer = new DataTransfer();
-        cy.get('@bunList').find('li').first().find('img').as('bun')
-        cy.get('@sauceList').find('li').first().find('img').as('sauce')
-        cy.get('@mainList').find('li').first().find('img').as('main')
+        cy.get('@testedBun').find('img').as('bun')
+        cy.get('@testedSauce').find('img').as('sauce')
+        cy.get('@testedMain').find('img').as('main')
 
 
         cy.get('section').last().as('dropSection')
@@ -32,6 +40,7 @@ describe('burger-constructor e2e', ()=> {
         cy.get('@dropSection').trigger('drop', {
             dataTransfer
         });
+        cy.get('@testedBun').find("[class^=counter__num]").should('contain', 2)
 
         cy.get('@sauce').trigger('dragstart', {
             dataTransfer
@@ -39,13 +48,25 @@ describe('burger-constructor e2e', ()=> {
         cy.get('@dropSection').trigger('drop', {
             dataTransfer
         });
+
+
+        cy.get('@testedSauce').find("[class^=counter__num]").should('contain', 1)
+
+
         cy.get('@main').trigger('dragstart', {
             dataTransfer
         })
         cy.get('@dropSection').trigger('drop', {
             dataTransfer
         });
+        cy.get('@testedMain').find("[class^=counter__num]").should('contain', 1)
+
+        cy.get('@dropSection').find('ul').as('mainList')
+        cy.get('@mainList').children().should('have.length', 2)
 
         cy.get("button").contains('Оформить заказ').click()
+        cy.contains('идентификатор заказа')
+        cy.get('button').last().click()
+        cy.contains('идентификатор заказа').should('not.exist')
     })
 })
